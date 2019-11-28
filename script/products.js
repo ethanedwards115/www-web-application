f92c4154cb267a772a4f28e7be8f719f78e89ae6
Ethan Edwards <ethanedwards115@gmail.com>
2019-11-28 12:10:20 +0000

    Added script to products page

---
+++ script/products.js
@@ -0,0 +1,46 @@
+var products = {
+
+  categories: {
+    beansAndPulses: [
+      {
+        name: "Black turtle beans",
+        stock: "0.00kg",
+        price: "£0.00/kg"
+      },
+      {
+        name: "Black eye beans",
+        stock: "0.00kg",
+        price: "£0.00/kg"
+      },
+      {
+        name: "Broth mix",
+        stock: "0.00kg",
+        price: "£0.00/kg"
+      },
+      {
+        name: "Butter beans",
+        stock: "0.00kg",
+        price: "£0.00/kg"
+      }
+    ],
+    cereal: [],
+    driedFruit: [],
+    grains: [],
+    rice: [],
+    pasta: [],
+    nuts: [],
+    veganPickAndMix: [
+      {
+        name: "Vegan pick and mix",
+        stock: "0.00kg",
+        price: "£0.00/kg"
+      }
+    ],
+    snacks: [],
+    baking: [],
+    herbsAndSpices: [],
+    tea: [],
+    glutenFree: [],
+    dogBiscuits: []
+  }
+};
