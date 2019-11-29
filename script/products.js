801eaaab107c8fcee6707266eb6e0e31b6c1da1a
Ethan Edwards <ethanedwards115@gmail.com>
2019-11-29 16:36:04 +0000

    Scripts being made, commiting for stash

--- script/products.js
+++ script/products.js
@@ -1,133 +1,522 @@
 var products = {
 
-  categories: [
+  items: [
     [{
         category: "Beans and pulses",
         name: "Black turtle beans",
         img: "",
         stock: "0.00kg",
-        price: "£0.00/kg"
+        price: "£2.95/kg"
       },
       {
         category: "Beans and pulses",
         name: "Black eye beans",
         img: "",
         stock: "0.00kg",
-        price: "£0.00/kg"
+        price: "£2.95/kg"
       },
       {
         category: "Beans and pulses",
         name: "Broth mix",
         img: "",
         stock: "0.00kg",
-        price: "£0.00/kg"
+        price: "£2.95/kg"
       },
       {
         category: "Beans and pulses",
         name: "Butter beans",
         img: "",
         stock: "0.00kg",
-        price: "£0.00/kg"
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Chick peas",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Red kidney beans",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Red lentils",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Marrowfat peas",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Pinto beans",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Green lentils",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "Beans and pulses",
+        name: "Yellow split peas",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
       }
     ],
     [{
-      category: "cereal",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "cereal",
+        name: "Deluxe muesli",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "cereal",
+        name: "Exotic muesli",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "cereal",
+        name: "Banana bran",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "cereal",
+        name: "Porridge oats",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
-      category: "dried fruit",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "dried fruit",
+        name: "Apple rings",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Apricots",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Bana chips",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Dates (pitted)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Cranberries (added sugar)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Papaya (diced, added sugar)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Pineapple (diced, added sugar)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Raisins",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "dried fruit",
+        name: "Goji berries",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
-      category: "grains",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "grains",
+        name: "Pearl cous cous",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "grains",
+        name: "Popcorn",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "grains",
+        name: "Quinoa",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "grains",
+        name: "Tri colour quinoa",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "grains",
+        name: "White cous cous",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
-      category: "rice",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "rice",
+        name: "Arborio rice",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "rice",
+        name: "Long grain brown rice",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "rice",
+        name: "Long grain white rice (organic)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "rice",
+        name: "White basmati rice",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
-      category: "pasta",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "pasta",
+        name: "Lucky Boat noodles",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "pasta",
+        name: "White macaroni pasta",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "pasta",
+        name: "White spaghetti",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "pasta",
+        name: "Whole-wheat pasta",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
-      category: "nuts",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-
-    }],
+        category: "nuts",
+        name: "Almonds (whole)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "nuts",
+        name: "Cashews (broken)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "nuts",
+        name: "Hazelnuts (whole, roasted)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "nuts",
+        name: "Peanuts (redskin)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "nuts",
+        name: "Peanuts (roasted <em>and</em> salted)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "nuts",
+        name: "Peanuts (roasted)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "nuts",
+        name: "Pistachios (roasted <em>and</em> salted)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
       category: "vegan pick and mix",
       name: "Vegan pick and mix",
       stock: "0.00kg",
-      price: "£0.00/kg"
+      price: "£2.95/kg"
     }],
     [{
-      category: "snacks",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "snacks",
+        name: "Bombay mix",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Rice cracker osumi mix",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Chilli rice crackers",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Garlic sticks",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Dark chocolate ginger",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Dark chocolate raisins",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Dark chocolate brazils",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Milk chocolate banana chips",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Milk chocolate brazils",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Yoghurt & pineapple",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Yoghurt & banana chips",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Yoghurt & apricots",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "snacks",
+        name: "Yoghurt & almonds",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
-      category: "baking",
-      name: "xxx",
-      img: "",
-      stock: "0.00kg",
-      price: "£0.00/kg"
-    }],
+        category: "baking",
+        name: "B12 yeast flakes",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "baking",
+        name: "Icing sugar (organic)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "baking",
+        name: "White caaster sugar (refined)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "baking",
+        name: "Sea salt crystals",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "baking",
+        name: "Cocoa powder (organic)",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      },
+      {
+        category: "baking",
+        name: "Baking powder",
+        img: "",
+        stock: "0.00kg",
+        price: "£2.95/kg"
+      }
+    ],
     [{
       category: "herbs and spices",
       name: "xxx",
       img: "",
       stock: "0.00kg",
-      price: "£0.00/kg"
+      price: "£2.95/kg"
     }],
     [{
       category: "tea",
       name: "xxx",
       img: "",
       stock: "0.00kg",
-      price: "£0.00/kg"
+      price: "£2.95/kg"
     }],
     [{
       category: "gluten-free",
       name: "xxx",
       img: "",
       stock: "0.00kg",
-      price: "£0.00/kg"
+      price: "£2.95/kg"
     }],
     [{
       category: "dog biscuits",
       name: "xxx",
       img: "",
       stock: "0.00kg",
-      price: "£0.00/kg"
+      price: "£2.95/kg"
     }]
   ]
 };
 
 console.log(products);
 
+function createNode(element) {
+  return document.createElement(element);
+}
+
 function append(parent, element) {
   parent.appendChild(element);
+}
+
+function setFilterTags() {
+  var promise = Promise.resolve(products);
+
+  var container = document.getElementById("filter-tags");
+
+  promise.then(function(json) {
+      return json.items;
+    })
+    .then(function(items) {
+
+      for (var i = 0; i < items.length; i++) {
+        var tag = document.createElement("a");
+        tag.setAttribute("class", "dropdown-item");
+
+        tag.innerHTML = items[i][0].category;
+
+        append(container, tag);
+      }
+    })
 }
 
 function displayProducts() {
@@ -137,32 +526,44 @@ function displayProducts() {
   var container = document.getElementById("products-container");
 
   promise.then(function(json) {
-      return json.categories;
+      return json.items;
     })
     .then(function(items) {
 
       for (var i = 0; i < items.length; i++) {
-        for(var j = 0; j < items[i].length; j++){
+        for (var j = 0; j < items[i].length; j++) {
 
-          var item = document.createElement("li");
-          var name = document.createElement("h3");
-          var stock = document.createElement("p");
-          var price = document.createElement("p");
-          item.setAttribute("class", "list-group-item");
+          let li = createNode('li'),
+            div1 = createNode('div'),
+            img = createNode('img'),
+            div2 = createNode('div'),
+            name = createNode('h5'),
+            stock = createNode('p'),
+            price = createNode('p'),
+            span = createNode('span');
 
-          name.innerHTML = items[i][j].name;
-          stock.innerHTML = "Stock remaining: " + items[i][j].stock;
-          price.innerHTML = "Price: " + items[i][j].price;
+          img.src = items[i][j].img;
+          h5.innerHTML = `${movie.Title}`;
 
-          append(item, name);
-          append(item, stock);
-          append(item, price);
+          div1.setAttribute('class', 'card');
+          div1.setAttribute('style', 'width: 18rem;');
+          img.setAttribute('class', 'card-img-top');
+          div2.setAttribute('class', 'card-body');
+          h5.setAttribute('class', 'card-title');
+          p.setAttribute('class', 'card-text');
 
-          append(container, item);
+          append(li, div1);
+          append(div1, img);
+          append(div1, div2);
+          append(div2, h5);
+          append(div2, p);
+
+          append(li, span);
+          append(ul, li);
         }
       }
     });
 }
 
-
+setFilterTags();
 displayProducts();
